e504d2a1c54aaa91e11f5df31e966b3cc53a8d50
Sumit Bhandari <139847765+DLI0213-SumitBhandari@users.noreply.github.com>
2026-08-13 22:32:01 +0530

    Time: 107 ms (51.56%), Space: 150.2 MB (29.69%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.java
@@ -1,87 +1,153 @@
 class Solution {
-    static class Node {char leftChar; char rightChar; int length; int prefix; int suffix;
-int best;
-        Node(char leftChar, char rightChar, int length, int prefix, int suffix, int best) {
-            this.leftChar = leftChar;
-            this.rightChar = rightChar;
-            this.length = length;
-            this.prefix = prefix;
-            this.suffix = suffix;
-            this.best = best;
+        public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
+        LongestSubstringSegmentTree segmentTree = new LongestSubstringSegmentTree(s);
+        char[] source = s.toCharArray();
+        int queryLength = queryIndices.length;
+        int[] longestSubstring = new int[queryLength];
+
+        for (int i = 0; i < queryLength; i++) {
+            int index = queryIndices[i];
+
+            char currChar = source[index];
+            char newChar = queryCharacters.charAt(i);
+
+            if (newChar != currChar) {
+                source[index] = newChar;
+                segmentTree.update(index, newChar);
+            }
+
+            longestSubstring[i] = segmentTree.query(0, s.length() - 1).substring;
         }
+
+        return longestSubstring;
     }
 
-    private Node[] tree;
+    static class LongestSubstringSegmentTree {
 
-    private Node merge(Node left, Node right) {
-        int length = left.length + right.length;
-        int prefix = left.prefix;
+        private static class Node {
 
-        if (left.rightChar == right.leftChar && left.prefix == left.length) {
-            prefix = left.length + right.prefix;
+            int substring;
+            int prefix;
+            int suffix;
+            boolean isEmpty;
+
+            public Node(int longestSubstring, int longestPrefixSubstring, int longestSuffixSubstring, boolean isEmpty) {
+                this.substring = longestSubstring;
+                this.prefix = longestPrefixSubstring;
+                this.suffix = longestSuffixSubstring;
+                this.isEmpty = isEmpty;
+            }
+
+            public static Node getEmpty() {
+                return new Node(0, 0, 0, true);
+            }
+
+            public static Node singleNode(int val) {
+                return new Node(val, val, val, false);
+            }
+
+            public boolean getIsEmpty() {
+                return isEmpty;
+            }
         }
 
-        int suffix = right.suffix;
+        private final int size;
+        private final Node[] tree;
+        private final char[] arr;
 
-        if (left.rightChar == right.leftChar && right.suffix == right.length) {
-            suffix = right.length + left.suffix;
+        public LongestSubstringSegmentTree(String s) {
+            this.size = s.length();
+            this.tree = new Node[size << 2];
+            this.arr = s.toCharArray();
+
+            build(0, 0, size - 1);
         }
 
-        int best = Math.max(left.best, right.best);
-
-        if (left.rightChar == right.leftChar) {
-            best = Math.max(best, left.suffix + right.prefix);
+        // Returns the maximum subarray sum in the inclusive range [left, right].
+        private Node query(int queryLeft, int queryRight) {
+            return query(0, 0, size - 1, queryLeft, queryRight);
         }
 
-        return new Node(left.leftChar, right.rightChar, length, prefix, suffix, best);
-    }
-
-    private void build( int node, int start, int end, String s) {
-        if (start == end) {
-            tree[node] = new Node( s.charAt(start), s.charAt(start), 1, 1, 1, 1);
-            return;
+        // Updates whole tree based on this new value at the given index.
+        private void update(int index, char ch) {
+            arr[index] = ch;
+            update(0, 0, size - 1, index, ch);
         }
 
-        int mid = (start + end) / 2;
+        private Node query(int nodeIndex, int left, int right, int queryLeft, int queryRight) {
+            if (queryLeft <= left && queryRight >= right) return tree[nodeIndex];
+            if (queryLeft > right || queryRight < left) return Node.getEmpty();
 
-        build(node * 2, start, mid, s);
-        build(node * 2 + 1, mid + 1, end, s);
+            int mid = left + ((right - left) >> 1);
 
-        tree[node] = merge(tree[node * 2],tree[node * 2 + 1]
-        );
-    }
+            int leftChild = nodeIndex * 2 + 1;
+            int rightChild = nodeIndex * 2 + 2;
 
-    private void update( int node, int start, int end, int index, char ch) {
-        if (start == end) {
-            tree[node] = new Node(ch, ch, 1, 1, 1, 1);
-            return;
+            Node leftResult = query(leftChild, left, mid, queryLeft, queryRight);
+            Node rightResult = query(rightChild, mid + 1, right, queryLeft, queryRight);
+
+            return merge(leftResult, rightResult, left, right);
         }
 
-        int mid = (start + end) / 2;
 
-        if (index <= mid) {
-            update(node * 2, start, mid, index, ch);
-        } else {
-            update(node * 2 + 1, mid + 1, end, index, ch);
+        private void update(int nodeIndex, int left, int right, int index, char newChar) {
+            if (left == right) {
+                // nothing to change as we are not storing char as leaf nodes
+                return;
+            }
+
+            int mid = left + ((right - left) >> 1);
+            int leftChild = 2 * nodeIndex + 1;
+            int rightChild = 2 * nodeIndex + 2;
+
+            if (index <= mid) update(leftChild, left, mid, index, newChar);
+            else update(rightChild, mid + 1, right, index, newChar);
+
+            tree[nodeIndex] = merge(tree[leftChild], tree[rightChild], left, right);
         }
 
-        tree[node] = merge(
-            tree[node * 2],
-            tree[node * 2 + 1]
-        );
-    }
+        private Node merge(Node left, Node right, int start, int end) {
+            if (left.isEmpty) return right;
+            if (right.isEmpty) return left;
 
-    public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
-        int n = s.length();
-        tree = new Node[4 * n];
-        build(1, 0, n - 1, s);
-        int[] answer = new int[queryIndices.length];
+            int mid = start + ((end - start) >> 1);
+            int leftLen = mid - start + 1;
+            int rightLen = end - mid;
+            boolean edgesMatch = arr[mid] == arr[mid + 1];
+            // 0000 0000
+            int longestSubstring = Math.max(left.substring, right.substring);
+            if (edgesMatch) { // merging points have same char, then left's suffix and right's prefix can be considered
+                longestSubstring = Math.max(longestSubstring, left.suffix + right.prefix);
+            }
 
-        for (int i = 0; i < queryIndices.length; i++) {
-            update(1, 0, n - 1, queryIndices[i], queryCharacters.charAt(i));
-            answer[i] = tree[1].best;
+            int longestPrefixSubstring = left.prefix;
+            if (edgesMatch && left.prefix == leftLen) {
+                longestPrefixSubstring += right.prefix;
+            }
+
+            int longestSuffixSubstring = right.suffix;
+            if (edgesMatch && right.suffix == rightLen) {
+                longestSuffixSubstring += left.suffix;
+            }
+
+            return new Node(longestSubstring, longestPrefixSubstring, longestSuffixSubstring, false);
         }
 
-        return answer;
+        private void build(int nodeIndex, int left, int right) {
+            if (left == right) {
+                tree[nodeIndex] = Node.singleNode(1);
+                return;
+            }
+
+            int mid = left + ((right - left) >> 1);
+
+            int leftChild = 2 * nodeIndex + 1;
+            int rightChild = 2 * nodeIndex + 2;
+
+            build(leftChild, left, mid);
+            build(rightChild, mid + 1, right);
+
+            tree[nodeIndex] = merge(tree[leftChild], tree[rightChild], left, right);
+        }
     }
 }
